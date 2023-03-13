735ca2f6188c576d9b7c3cfbec3f17c99a33b83d
sean00880 <theelitereapersanctum@gmail.com>
2023-03-12 22:15:40 -0400

    10th

--- components/Hero/index.jsx
+++ components/Hero/index.jsx
@@ -19,7 +19,7 @@ export default function Hero({
   return (
     <Div
       className="cs-hero cs-style1 cs-bg cs-fixed_bg cs-shape_wrap_1"
-      style={{ backgroundImage: `url(${bgImageUrl})` }}
+      style={{ backgroundImage: '/images/sky.jpg' }}
     >
       <Div className="cs-shape_1" />
       <Div className="cs-shape_1" />
