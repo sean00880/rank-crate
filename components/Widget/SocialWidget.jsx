d1bda5f5f2643368dd7d3a16987aefc5f46778ee
sean00880 <theelitereapersanctum@gmail.com>
2023-03-13 23:43:30 -0400

    4th commit

--- components/Widget/SocialWidget.jsx
+++ components/Widget/SocialWidget.jsx
@@ -6,17 +6,9 @@ export default function SocialWidget() {
   return (
     <Div className="cs-social_btns cs-style1">
       <Link href="/" className="cs-center">
-        <Icon icon="fa6-brands:linkedin-in" />
+        <Icon icon="fa6-brands:reddit" />
       </Link>
-      <Link href="/" className="cs-center">
-        <Icon icon="fa6-brands:twitter" />
-      </Link>
-      <Link href="/" className="cs-center">
-        <Icon icon="fa6-brands:youtube" />
-      </Link>
-      <Link href="/" className="cs-center">
-        <Icon icon="fa6-brands:slack" />
-      </Link>
+  
     </Div>
   );
 }
